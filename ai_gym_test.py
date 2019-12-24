0dfddfb1d80be4b7717f23c947c6d13dc8825953
Sean <sean@cortirio.com>
2019-12-24 16:02:27 +0000

    AI gym CartPole-v1 - commit before converting to class

--- ai_gym_test.py
+++ ai_gym_test.py
@@ -1,14 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import gym
+import matplotlib.pyplot as plt
+import numpy as np
 np.set_printoptions(precision=1, linewidth=np.inf)
 
-
-# TODO: Include angular velocity (p_vel)
-
-
 env = gym.make('CartPole-v1')
-env.reset()
+initial_observation = env.reset()
 
 ob_dict = {
     'pos': 0,
@@ -17,13 +13,12 @@ ob_dict = {
     'p_vel': 3
 }
 
-ANGLE_MIN, ANGLE_MAX = -5.0, 5.0
-# POLE_VEL_MIN, POLE_VEL_MAX = -2.0, 3.0
-NUM_BINS = 1000
-BIN_SIZE = (ANGLE_MAX - ANGLE_MIN) / NUM_BINS
-learning_rate = 0.2
-discount = 0.99
-EPISODES = 100000
+PARAM_MIN, PARAM_MAX = -5.0, 5.0
+NUM_BINS = 100
+BIN_SIZE = (PARAM_MAX - PARAM_MIN) / NUM_BINS
+learning_rate = 0.7
+discount = 0.9
+EPISODES = 1000
 
 epsilon = 1.0
 START_EPSILON_DECAYING = 1
@@ -32,7 +27,9 @@ epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 
 def update_q_value(learning_rate, current_q, reward, discount, max_future_q):
-    return (1 - learning_rate) * current_q + learning_rate * (reward + discount * max_future_q)
+    new_q = (1 - learning_rate) * current_q + learning_rate * (reward + discount * max_future_q)
+    print('q, update:', current_q, new_q)
+    return new_q
 
 
 def get_bin_index(value, min_value, bin_size):
@@ -43,18 +40,16 @@ def get_bin_value(index, min_value, bin_size):
     return min_value + index * bin_size
 
 
-# Create & initialise a q value table # TODO: Extend this by num bins of angular velocity
-q_table = np.random.uniform(low=-0.1, high=0.1, size=(2, NUM_BINS))  # actions, states (angles)
-
+# Create & initialise a q value table
+q_table = np.random.uniform(low=0, high=10, size=(2, NUM_BINS, NUM_BINS))  # actions, pole angle, pole velocity
 print('q table initial\n', q_table)
 
 # Get the initial state
 action = 1
-ob_old, reward_old, done, _ = env.step(action)
-print('rewared_old', reward_old)
+ob_current, reward_current, done, _ = env.step(action)
+print('rewared_current', reward_current)
 
 num_steps_alive = 0
-num_random = 0
 num_alive = []
 for episode in range(EPISODES):
     # print('episiode', episode)
@@ -67,48 +62,53 @@ for episode in range(EPISODES):
 
     while not done:
 
-        # Find the action providing the maximum q value # TODO: Use angle and angular velocity (get 2 indicies)
-        ob_old_index = get_bin_index(ob_old[ob_dict['angle']], ANGLE_MIN, BIN_SIZE)
+        # Find the action providing the maximum q value
+        # First get the q-table indicies for the current observation
+        ob_current_index_angle = get_bin_index(ob_current[ob_dict['angle']], PARAM_MIN, BIN_SIZE)
+        ob_current_index_p_vel = get_bin_index(ob_current[ob_dict['p_vel']], PARAM_MIN, BIN_SIZE)
 
+        # Look up in the q-table the action corresponding to the highest q-value given the current observation
+        # Instead of this, choose a random action with a probability 1-epsilon to aid exploration
         if np.random.random() > epsilon:
-            action_for_max_q = np.argmax(q_table[:, ob_old_index])  # TODO: 2 indicies in q_table lookup
+            action_for_max_q = np.argmax(q_table[:, ob_current_index_angle, ob_current_index_p_vel])
+            print('action_for_max_q', action_for_max_q)
         else:
             action_for_max_q = np.random.randint(0, 1)
-            num_random += 1
 
-        # Play a step in the simulation with this optimal value to get a new observation
-        ob_new, reward_new, done, _ = env.step(action_for_max_q)
+        # Play a step in the simulation with this optimal value to get a future observation
+        ob_future, reward_future, done, _ = env.step(action_for_max_q)
         if done:
             num_alive.append(num_steps_alive)
-            print(num_steps_alive)
-            reward_new = 0
+            # print(num_steps_alive)
+            reward_future = -10
             # break
         else:
             num_steps_alive += 1
-            reward_new = 1
+            reward_future = 1
 
-        ob_new_index = get_bin_index(ob_new[ob_dict['angle']], ANGLE_MIN, BIN_SIZE)  # TODO: 2 incicies
+        ob_future_index_angle = get_bin_index(ob_future[ob_dict['angle']], PARAM_MIN, BIN_SIZE)
+        ob_future_index_p_vel = get_bin_index(ob_future[ob_dict['p_vel']], PARAM_MIN, BIN_SIZE)
 
         # Get max future q
-        max_future_q = np.max(q_table[:, ob_new_index])
+        max_future_q = np.max(q_table[:, ob_future_index_angle, ob_future_index_p_vel])
 
         # Update the table q value
-        new_q_value = update_q_value(learning_rate,
-                                     q_table[action_for_max_q, ob_old_index],
-                                     reward_new,
+        future_q_value = update_q_value(learning_rate,
+                                     q_table[action_for_max_q, ob_current_index_angle, ob_current_index_p_vel],
+                                     reward_future,
                                      discount,
                                      max_future_q)
-        q_table[action, ob_old_index] = new_q_value
+        q_table[action, ob_current_index_angle, ob_current_index_p_vel] = future_q_value
         # print('q table after {} steps\n'.format(i), q_table)
 
-        ob_old = ob_new
-        reward_old = reward_new
+        ob_current = ob_future
+        reward_current = reward_future
         action = action_for_max_q
 
     env.reset()
-    ob_old, reward_old, done, _ = env.step(1)
+    ob_current, reward_current, done, _ = env.step(1)
 
-    if not episode % 1000:
+    if not episode % 100000:
         np.save('./data/{}'.format(episode), q_table)
 
 print(q_table)
@@ -117,3 +117,4 @@ print(num_alive)
 
 plt.plot(num_alive)
 plt.show()
+
--- use_model.py
+++ use_model.py
@@ -11,8 +11,8 @@ ob_dict = {
 }
 
 ANGLE_MIN, ANGLE_MAX = -5.0, 5.0
-# POLE_VEL_MIN, POLE_VEL_MAX = -2.0, 3.0
-NUM_BINS = 1000
+POLE_VEL_MIN, POLE_VEL_MAX = -5.0, 5.0
+NUM_BINS = 100
 BIN_SIZE = (ANGLE_MAX - ANGLE_MIN) / NUM_BINS
 
 
@@ -25,14 +25,14 @@ def get_bin_value(index, min_value, bin_size):
 
 
 # action, angle
-q_table = np.load('./data/90000.npy')
+q_table = np.load('./data/9900000.npy')
 # print(q_table.shape)
 
 action = 1
 num_steps_alive = 0
 while True:
     env.render()
-    # input('continue...')
+    input('continue...')
 
     # Play a step in the simulation with this optimal value to get a new observation
     ob_new, reward_new, done, _ = env.step(action)
@@ -44,8 +44,9 @@ while True:
 
     # Get the index in the q-table for the observed angle
     angle_bin_index = get_bin_index(ob_new[ob_dict['angle']], ANGLE_MIN, BIN_SIZE)
+    p_vel_bin_index = get_bin_index(ob_new[ob_dict['p_vel']], ANGLE_MIN, BIN_SIZE)
     # Get the action in the q-table with the highest q-value
-    action = np.argmax(q_table[:, angle_bin_index])
+    action = np.argmax(q_table[:, angle_bin_index, p_vel_bin_index])
 
 env.close()
 print(num_steps_alive)
